522f0dea082906c24dd259051989c0b76978ff76
Rick Lionel Onana <45133928+ricko97@users.noreply.github.com>
2023-09-15 22:02:50 -0400

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,6 +5,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -26,7 +27,9 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <HStack>
-          <Heading fontSize="2xl">{game.name}</Heading>
+          <Heading fontSize="2xl">
+            <Link to={"/games/" + game.slug}>{game.name}</Link>
+          </Heading>
           <Emoji rating={game.rating_top} />
         </HStack>
       </CardBody>
