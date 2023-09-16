4b8f9c5b8d3e0ca1f4cdffa0167e57252cdf9e4b
Rick Lionel Onana <45133928+ricko97@users.noreply.github.com>
2023-09-15 22:10:10 -0400

    Refactor- move entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,7 +10,7 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 
-import { Platform } from "../hooks/usePlatforms";
+import { Platform } from "../entities/Platform";
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
